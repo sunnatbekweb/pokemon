88eedf4d43af7fae6e3ddedcd953b055e2b7f4ae
sunnatbek07 <sunnatbekqadamov9@gmail.com>
2023-07-09 08:52:02 +0500

    added responsive

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const render = (arr) => {
     let res = "";
     arr.map((value) => {
         res += `
-        <div class="card bg-white w-[310px] rounded-[28px] border-2 border-black">
+        <div class="card bg-white w-[310px] rounded-[28px] border-2 border-black hover:shadow-xl transition-all">
             <div class="border-b-2 border-black">
                 <img src="${value.img}" class="mx-auto mt-8 mb-[66px]" alt="image">
             </div>
@@ -16,7 +16,7 @@ const render = (arr) => {
                 <p class="font-medium text-xl mb-[25px]">${value.type}</p>
                 <div class="flex justify-between">
                     <pc class="font-bold text-2xl">${value.weight}</pc>
-                    <p class="font-bold text-2xl">${value.height}</p>
+                    <p class="font-bold text-2xl">${value.candy_count}</p>
                 </div>
             </div>
         </div>
@@ -38,3 +38,23 @@ search.addEventListener("input", (e) => {
 })
 
 // ----------------------- Search end -------------------------
+
+// ----------------------- Filtered search ------------------------
+
+const gross = document.getElementById("filter")
+
+let ty = [];
+pokemons.forEach((e) => {
+    for (const iterator of e.type) {
+        ty.push(iterator)
+    }
+})
+
+const types = [...new Set(ty)];
+
+types.forEach((v) => {
+    let opt = document.createElement("option");
+    opt.value = v;
+    opt.textContent = v;
+    gross.append(opt);
+})
